7f0783d3fa841e6598afc1f1efbcf617ef638791
skumar <skumar@carltonone.com>
2021-06-05 19:51:14 -0400

    pm2 implement

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps : [
+      {
+      name: "loopback4",
+      cwd: "/home/node/library-loopback4",
+      script: "npm",
+      args: "start"
+    },
+    {
+      name: "vue3",
+      cwd: "/home/node/library-vue3",
+      script: "npm",
+      args: "run dev"
+    }
+  ]
+}
